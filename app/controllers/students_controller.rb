24302b1c2825d5653a6bcba3feff37c8ae7b501e
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-28 12:04:58 +0300

    commit tree 2c6e1406b820243ead766d43ba93c79556ba1c23

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students 
+    end
+    def grades
+        students = Student.all.order grade: :desc
+        render json: students
+    end
+    def highest_grade
+        student = Student.all.order(grade: :desc).limit(1)[0]
+        render json: student
+    end
+end
